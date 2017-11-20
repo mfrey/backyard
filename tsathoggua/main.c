1df5d84ebe60de087b69877d4ff4aac8ce0a834d
Michael Frey <michael.frey@msasafety.com>
2017-11-20 15:40:03 +0100

    added constants for tls initialization

---
+++ tsathoggua/main.c
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2017 MSA Safety 
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     examples
+ * @{
+ *
+ * @file
+ * @brief       TODO
+ *
+ * @author      Michael Frey <michael.frey@msasafety.com>
+ *
+ * @}
+ */
+#include "shell.h"
+#include "shell_commands.h"
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
+
