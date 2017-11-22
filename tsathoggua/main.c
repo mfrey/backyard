d0d3555706044386eb7174abf1d1e63df1408d54
Michael Frey <michael.frey@msasafety.com>
2017-11-22 12:12:41 +0100

    added missing tlsf include

--- tsathoggua/main.c
+++ tsathoggua/main.c
@@ -18,10 +18,26 @@
  * @}
  */
 #include "shell.h"
+#include "tsathoggua.h"
 #include "shell_commands.h"
+
+#ifdef MODULE_TLSF
+#include "tlsf-malloc.h"
+#endif
+
+#ifdef MODULE_TLSF
+static uint32_t _tlsf_heap[TLSF_BUFFER];
+#endif
 
 int main(int argc, char **argv)
 {
+#ifdef MODULE_TLSF
+    tlsf_create_with_pool(_tlsf_heap, sizeof(_tlsf_heap));
+#endif
+
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
+
     return 0;
 }
 
