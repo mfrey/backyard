8b80d8cefb6a3be03c7e4d02b778c1eee0567dff
Michael Frey <michael.frey@msasafety.com>
2018-05-16 16:06:57 +0200

    fixed error in settings (default_content)

--- default_content/main.c
+++ default_content/main.c
@@ -22,28 +22,21 @@
 
 #include "msg.h"
 #include "shell.h"
-#include "shell_commands.h"
 
-#ifdef MODULE_TLSF
+#include "ccn-lite-riot.h"
 #include "tlsf-malloc.h"
-#endif
 
-#ifdef MODULE_TLSF
 #define TLSF_BUFFER     (10240 / sizeof(uint32_t))
 static uint32_t _tlsf_heap[TLSF_BUFFER];
-#endif
 
 /* main thread's message queue */
 #define MAIN_QUEUE_SIZE     (8)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
 
-int main(int argc, char **argv)
+int main(void)
 {
-    (void)argc;
-    (void)argv;
-#ifdef MODULE_TLSF
+    msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
     tlsf_create_with_pool(_tlsf_heap, sizeof(_tlsf_heap));
-#endif
     msg_init_queue(_main_msg_queue, MAIN_QUEUE_SIZE);
 
     /** initialze ccn-lite */
@@ -61,6 +54,7 @@ int main(int argc, char **argv)
     set_radio_settings(channel, tx_power, src_len, cca);
 
     /** run shell */
+
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
 
--- default_content/settings.c
+++ default_content/settings.c
@@ -1,25 +1,29 @@
 
 #include "settings.h"
 
+#include <stdio.h>
+
+#include "ccn-lite-riot.h"
+
 int set_radio_settings(uint16_t channel, uint16_t tx_power, uint16_t src_len, netopt_enable_t cca){
     /* get the default interface */
     gnrc_netif_t *netif;
 
     /* set the relay's PID, configure the interface to use CCN nettype */
     if (((netif = gnrc_netif_iter(NULL)) == NULL) ||
-        (ccnl_open_netif(netif->pid, GNRC_NETTYPE_CCN) < 0)) {
+            (ccnl_open_netif(netif->pid, GNRC_NETTYPE_CCN) < 0)) {
         puts("Error registering at network interface!");
         return -1;
     }
 
     /** set the channel */
-    gnrc_netapi_set(ifs[0], NETOPT_CHANNEL, 0, &channel, sizeof(channel));
+    gnrc_netapi_set(netif->pid, NETOPT_CHANNEL, 0, &channel, sizeof(channel));
     /** set the transmit power */
-    gnrc_netapi_set(ifs[0], NETOPT_TX_POWER, 0, &tx_power, sizeof(tx_power));
+    gnrc_netapi_set(netif->pid, NETOPT_TX_POWER, 0, &tx_power, sizeof(tx_power));
     /** enable/disable auto cca */
-    gnrc_netapi_set(ifs[0], NETOPT_AUTOCCA, 0, &cca, sizeof(netopt_enable_t));
+    gnrc_netapi_set(netif->pid, NETOPT_AUTOCCA, 0, &cca, sizeof(netopt_enable_t));
     /** set the size of the short addresses */
-    gnrc_netapi_set(ifs[0], NETOPT_SRC_LEN, 0, &src_len, sizeof(src_len));
+    gnrc_netapi_set(netif->pid, NETOPT_SRC_LEN, 0, &src_len, sizeof(src_len));
 
     return 0;
 }
