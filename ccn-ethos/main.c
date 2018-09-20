7e098c0b1fb12d18ea8d818efd8de168f940906e
Michael Frey <michael.frey@msasafety.com>
2018-09-20 15:45:09 +0200

    replaced functions for creating interests/content with RIOTs shell functions

--- ccn-ethos/main.c
+++ ccn-ethos/main.c
@@ -20,94 +20,25 @@
 
 #include "msg.h"
 #include "shell.h"
+#include "shell_commands.h"
 #include "net/gnrc/netif.h"
 #include "net/gnrc/pktdump.h"
 
 #include "ccn-lite-riot.h"
+#include "ccnl-pkt-builder.h"
 #include "tlsf-malloc.h"
 
-#define TLSF_BUFFER     (10240 / sizeof(uint32_t))
+#define TLSF_BUFFER     (4096 / sizeof(uint32_t))
 static uint32_t _tlsf_heap[TLSF_BUFFER];
 
+/** defined in sys/shell/commands/sc_ccnl.c */
+extern int _ccnl_interest(int argc, char **argv); 
+/** again, defined in sys/shell/commands/sc_ccnl.c */
+extern int _ccnl_content(int argc, char **argv);
+
 /* main thread's message queue */
-#define MAIN_QUEUE_SIZE (8)
+#define MAIN_QUEUE_SIZE (4)
 static msg_t _main_msg_queue[MAIN_QUEUE_SIZE];
-
-#define BUFFER_SIZE     (128u)
-/** */
-static unsigned char _int_buf[BUFFER_SIZE];
-/** */
-static unsigned char _out[CCNL_MAX_PACKET_SIZE];
-
-
-static struct ccnl_interest_s *create_interest(struct ccnl_relay_s *ccnl, unsigned char* name){
-    int suite = CCNL_SUITE_NDNTLV;
-    int nonce = random_uint32();
-
-    /** create prefix */
-    struct ccnl_prefix_s *prefix = ccnl_URItoPrefix(name, suite, NULL);
-    /** */
-    int len = ccnl_mkInterest(prefix, NULL, _int_buf, BUFFER_SIZE);
-    ccnl_prefix_free(prefix);
-
-    unsigned char *start = _int_buf;
-    unsigned char *data = _int_buf;
-
-    int type;
-    int int_len;
-
-    if (ccnl_ndntlv_dehead(&data, &len, (int*) &type, &int_len) || (int) int_len > len) {
-        printf("ccnl_helper: invalid packet format\n");
-        return NULL;
-    }
-
-    struct ccnl_pkt_s *pkt = ccnl_ndntlv_bytes2pkt(NDN_TLV_Interest, start, &data, &len);
-    struct ccnl_face_s *loopback_face = ccnl_get_face_or_create(&ccnl_relay, -1, NULL, 0);
-
-    return ccnl_interest_new(&ccnl_relay, loopback_face, &pkt);
-}
-
-/**
- * Creates a content object with the given \p name and \p payload and adds it to the content
- * store. The content is marked as 'static'
- *
- * @param[in] ccnl The global \ref ccnl_relay_s struct
- * @param[in] name The prefix of the content to add to the content store
- * @param[in] payload The data to add to the content store
- * @param[in] length The size of the \p payload
- *
- */
-static void create_content(struct ccnl_relay_s *ccnl, unsigned char* name, unsigned char *payload, int length){
-
-    int suite = CCNL_SUITE_NDNTLV;
-    int offset = CCNL_MAX_PACKET_SIZE;
-
-    /** create prefix */
-    struct ccnl_prefix_s *prefix = ccnl_URItoPrefix(name, suite, NULL);
-    int arg_len = ccnl_ndntlv_prependContent(prefix, payload,
-        length, NULL, NULL, &offset, _out);
-    ccnl_prefix_free(prefix);
-
-    int len;
-    unsigned type;
-    unsigned char *olddata;
-    unsigned char *data = olddata = _out + offset;
-
-    /***/
-    if (ccnl_ndntlv_dehead(&data, &arg_len, (int*) &type, &len) || type != NDN_TLV_Data) {
-        printf("ERROR in _store_\n");
-        return;
-    }
-
-    /** create content */
-    struct ccnl_content_s *content = NULL;
-    struct ccnl_pkt_s *packet = ccnl_ndntlv_bytes2pkt(type, olddata, &data, &arg_len);
-    content = ccnl_content_new(&packet);
-
-    /** mark content as static and add it to the cache */
-    content->flags |= CCNL_CONTENT_FLAGS_STATIC;
-    ccnl_cs_add(ccnl, content);
-}
 
 
 int main(void)
@@ -121,7 +52,7 @@ int main(void)
     /** run it */
     ccnl_start();
 
-//    create_interest(&ccnl_relay, "/test/data");
+    _ccnl_interest(10, (char **)"/test/data");
 
     /** run shell */
     char line_buf[SHELL_DEFAULT_BUFSIZE];
