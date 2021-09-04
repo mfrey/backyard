db1cbf9abf14a2a601420d41569c34ce436eae91
Michael Frey <mail@mfrey.net>
2021-09-04 15:08:40 +0200

    updated option and result exercise

--- rust/files/src/main.rs
+++ rust/files/src/main.rs
@@ -1,12 +1,19 @@
 use std::fs::File;
+use std::io::prelude::*;
 use std::path::Path;
 
 fn main() {
     let path = Path::new("src/data/hello.txt");
-    //let display = path.display();
 
     let mut file = match File::open(&path) {
         Err(why) => panic!("couldn't open {}: {}", path.display(), why),
         Ok(file) => file,
     };
+
+    let mut buffer = String::new();
+    match file.read_to_string(&mut buffer) {
+        Err(why) => panic!("coult not read file {}", why),
+        Ok(bytes_read) => println!("read {} bytes", bytes_read)
+    }
+    println!("{}", buffer);
 }
