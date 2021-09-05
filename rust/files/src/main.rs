e7f53cb3952f2c36e3073a06ae7ea98ebe801cdb
Michael Frey <mail@mfrey.net>
2021-09-05 16:55:15 +0200

    modified code and example for next option and result exercise

--- rust/files/src/main.rs
+++ rust/files/src/main.rs
@@ -1,19 +1,30 @@
 use std::fs::File;
 use std::io::prelude::*;
 use std::path::Path;
+use std::io::BufReader;
 
 fn main() {
     let path = Path::new("src/data/hello.txt");
 
-    let mut file = match File::open(&path) {
+    let file = match File::open(&path) {
         Err(why) => panic!("couldn't open {}: {}", path.display(), why),
         Ok(file) => file,
     };
 
-    let mut buffer = String::new();
-    match file.read_to_string(&mut buffer) {
-        Err(why) => panic!("coult not read file {}", why),
-        Ok(bytes_read) => println!("read {} bytes", bytes_read)
+    let reader = BufReader::new(file).lines();
+    let mut lines = 0;
+
+    for i in reader {
+       lines += 1;
+       match i {
+           Err(why) => panic!("could not read line {}, cause: {}", lines, why),
+           Ok(line) => {
+               if !line.is_empty() {
+                   println!("{}", line)
+               }
+           }
+       }
     }
-    println!("{}", buffer);
+
+    println!("total number of lines: {}", lines);
 }
